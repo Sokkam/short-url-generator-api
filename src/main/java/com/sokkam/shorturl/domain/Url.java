cef4da757de73f35181451a87c33cfa763e7cb84
sukeming <sukeming@ctirobot.com>
2018-08-14 18:57:04 +0800

    第一次提交，未完成，只添加了10进制转换62进制的方法

---
+++ src/main/java/com/sokkam/shorturl/domain/Url.java
@@ -0,0 +1,37 @@
+package com.sokkam.shorturl.domain;
+
+public class Url {
+
+    private int id;
+
+    // 域名
+    private String domainName;
+
+    // 原链接
+    private String originalUrl;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDomainName() {
+        return domainName;
+    }
+
+    public void setDomainName(String domainName) {
+        this.domainName = domainName;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+}
---
+++ src/main/java/com/sokkam/shorturl/utils/ShortUrlGenerator.java
@@ -0,0 +1,37 @@
+package com.sokkam.shorturl.utils;
+
+public class ShortUrlGenerator {
+
+    private static final int sextyTwoHex = 62;
+
+    // 0 - z 有62位
+    private static final char[] character = {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8',
+            '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+            'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+            'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+            'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+            's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    };
+
+    private static String changeSixtyTwoHex(long number) {
+        StringBuilder stringBuilder = new StringBuilder();
+        while ((number / sextyTwoHex) != 0) {
+            // 取余数
+            long remainder = number % sextyTwoHex;
+            // 根据余数取字符集里的字符，下标从0开始算
+            char c = character[(int) remainder];
+            stringBuilder.append(c);
+            // 取商数，在下一次循环继续使用
+            number = number / sextyTwoHex;
+        }
+        stringBuilder.append(character[(int) number % sextyTwoHex]);
+        return stringBuilder.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(changeSixtyTwoHex(1000000));
+    }
+
+}
