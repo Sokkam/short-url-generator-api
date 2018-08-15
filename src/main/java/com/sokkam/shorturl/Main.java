fc4f55abfac4e35654314539157a425c2741b1e7
sukeming <sukeming@ctirobot.com>
2018-08-15 11:34:44 +0800

    粗糙的短链接生成测试器

--- src/main/java/com/sokkam/shorturl/Main.java
+++ src/main/java/com/sokkam/shorturl/Main.java
@@ -7,9 +7,9 @@ public class Main {
     public static void main(String[] args) {
 
         // 原地址
-        final String url = "";
+        final String url = "http://www.skm.com/wfawfa98wf7y978y91";
         // 输出文件到哪
-        final String filePath = "";
+        final String filePath = "G:/";
         UrlParser parser = new UrlParser();
         String jsonContent = parser.storage(url, filePath);
         System.out.println(jsonContent);
